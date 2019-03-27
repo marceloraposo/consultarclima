a47b3eaccacf29483e3bdd7dc1d341989adb1946
Marcelo Raposo Cerqueira <marcelo.raposo@gmail.com>
2019-03-27 10:54:58 -0300

    config

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const port = 2019;
-const ip = '172.30.155.235';
+const ip = 'localhost';
 const appid = 'dcc4b4fa314f3b593cef80cfe637b113'; //chave para acessar serviço externo
 
 exports = module.exports = {
