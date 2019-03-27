8a9d03ae45f18f3601ab251b92dc84d769f260b2
Marcelo Raposo Cerqueira <marcelo.raposo@gmail.com>
2019-03-27 11:30:31 -0300

    config

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const port = 2019;
+const port = 8080;
 const ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 const appid = 'dcc4b4fa314f3b593cef80cfe637b113'; //chave para acessar serviço externo
 
